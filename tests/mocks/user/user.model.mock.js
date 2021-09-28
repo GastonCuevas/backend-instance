9be16e7136b5bef6d53bde5c0c9ec0391da07b2b
GastonCuevas <gaston.cuevas@INFOCORP1.onmicrosoft.com>
2021-09-28 11:24:33 -0300

    ADD: tests suites added

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: "507f191e810c19729de860ea",
+    name: "Gaston",
+    username: "Gaston",
+    password: "001203Gs"
+  },
+  users: [
+    {
+      _id: "507f191e810c19729de860ea",
+      name: "Gaston",
+      username: "Gaston",
+      password: "001203Gs"
+    },
+    {
+      _id: "507f191e810c19729de860eb",
+      name: "Carlos",
+      username: "Charlie",
+      password: "001203Gs"
+    }
+  ]
+};
